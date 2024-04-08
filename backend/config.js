0a2a186eef309575195bab35a545016c107c302d
boris <borissurgefinance@gmail.com>
2024-04-08 16:17:33 +0500

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://nameerfarooq18:root@mern-book-store.k8izzk3.mongodb.net/?retryWrites=true&w=majority&appName=MERN-book-store";
---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("WELCOME to the MERN stack tutorial");
+});
+
+//routes for saving a new book
+
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("APP CONNECTED TO DATABASE");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));

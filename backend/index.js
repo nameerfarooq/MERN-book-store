84eba9a031df57221272e939570c362e574ad2f3
nameerfarooq <nameerfarooquit@gmail.com>
2024-04-08 17:58:11 +0500

    Routes added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 const app = express();
-
+app.use(express.json());
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(200).send("WELCOME to the MERN stack tutorial");
@@ -18,16 +19,45 @@ app.post("/books", async (request, response) => {
       !request.body.publishYear
     ) {
       return response.status(400).send({
-        message:
-          "Send all required fields: title, author, publishYear",
+        message: "Send all required fields: title, author, publishYear",
       });
     }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
   }
 });
-
+//get all books
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      books,
+    });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send({ message: error.message });
+  }
+});
+//get book by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send({ message: error.message });
+  }
+});
 mongoose
   .connect(mongoDBURL)
   .then(() => {
